c22d3c38445f32e701c2f9e6f68c5a7dcdae1319
Roshan-Rawal <roshanrawal43@gmail.com>
2018-09-03 16:33:10 +0530

    first Commit

---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour {
+	public GameObject parentObject;
+	// Use this for initialization
+	void Start () {
+		transform.parent = parentObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision hit){
+		UnityEngine.Debug.Log(hit.transform.name);
+	}
+}
---
+++ Assets/Scripts/throwBalls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class throwBalls : MonoBehaviour {
+	public GameObject ballPrefab;
+	public GameObject parentObject;
+	public Camera arcamera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			GameObject temp = Instantiate(ballPrefab);
+			temp.transform.parent = parentObject.transform;
+			temp.transform.position = arcamera.transform.position;
+			temp.GetComponent<Rigidbody>().AddForce(new Vector3(0,1,1)*100f);
+		}
+	}
+}
